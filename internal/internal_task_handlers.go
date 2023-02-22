539b913e78c4485c97159a0a2d0595c41e6bd647
zhiyong.he <zhiyonghe@ZhiyongdeMacBook-Pro.local>
2023-02-22 09:59:37 +0800

    feat:WorkflowInfo with WorkflowStartTime field

--- internal/internal_task_handlers.go
+++ internal/internal_task_handlers.go
@@ -628,7 +628,9 @@ func (wth *workflowTaskHandlerImpl) createWorkflowContext(task *s.PollForDecisio
 			RunID: attributes.ParentWorkflowExecution.GetRunId(),
 		}
 	}
+	wfStartTime := time.Unix(0, h.Events[0].GetTimestamp())
 	workflowInfo := &WorkflowInfo{
+		WorkflowStartTime: wfStartTime,
 		WorkflowExecution: WorkflowExecution{
 			ID:    workflowID,
 			RunID: runID,
@@ -650,7 +652,6 @@ func (wth *workflowTaskHandlerImpl) createWorkflowContext(task *s.PollForDecisio
 		RetryPolicy:                         attributes.RetryPolicy,
 	}
 
-	wfStartTime := time.Unix(0, h.Events[0].GetTimestamp())
 	return newWorkflowExecutionContext(wfStartTime, workflowInfo, wth), nil
 }
 
@@ -1463,9 +1464,10 @@ func isSearchAttributesMatched(attrFromEvent, attrFromDecision *s.SearchAttribut
 }
 
 // return true if the check fails:
-//    domain is not empty in decision
-//    and domain is not replayDomain
-//    and domains unmatch in decision and events
+//
+//	domain is not empty in decision
+//	and domain is not replayDomain
+//	and domains unmatch in decision and events
 func checkDomainsInDecisionAndEvent(eventDomainName, decisionDomainName string) bool {
 	if decisionDomainName == "" || IsReplayDomain(decisionDomainName) {
 		return false
